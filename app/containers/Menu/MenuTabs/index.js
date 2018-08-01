7f020f04353f458a77184c3b664efa6a1b7595ca
ioanaChi <ioana_chichi@outlook.com>
2018-08-01 17:09:31 +0300

    fixed space bugs

--- app/containers/Menu/MenuTabs/index.js
+++ app/containers/Menu/MenuTabs/index.js
@@ -1,24 +1,22 @@
 import * as React from 'react';
 import styles from './style.scss';
 
-
-
 const MenuTabs = () => (
-    <div >
-            <div className={styles.tabsContainer}>
-                <div className={styles.projectsRew}>
-                    <p>Projects for review</p>
-                </div>
-                <div className={styles.upcoming}>
-                    <p>Upcoming LMS</p>
-                </div>
-                <div className={styles.syllabus}>
-                    <p>Syllabus</p>
-                </div>
-                <div className={styles.myVid}>
-                    <p>My Videos</p>
-                </div>
-        </div>
+  <div>
+    <div className={styles.tabsContainer}>
+      <div>
+        <p>Projects for review</p>
+      </div>
+      <div>
+        <p>Upcoming LMS</p>
+      </div>
+      <div>
+        <p>Syllabus</p>
+      </div>
+      <div>
+        <p>My Videos</p>
+      </div>
     </div>
-)
+  </div>
+);
 export default MenuTabs;
